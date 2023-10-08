006817492b5d087b4cab2be7dcc843f01d3b3936
Muhammad Usman <tmusman5@gmail.com>
2023-10-08 15:25:54 +0500

    changes: standardizing ISO into UTC
    changes: SalesDataResponse model

--- app/models.py
+++ app/models.py
@@ -1,20 +1,53 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+
 from datetime import datetime
+import pytz
 
 
 class SalesDataRequest(BaseModel):
-    start_date: Optional[datetime] = None
-    end_date: Optional[datetime] = None
+    start_date: Optional[str] = None
+    end_date: Optional[str] = None
     product: Optional[str] = None
     category: Optional[str] = None
+    limit: Optional[int] = 10
+    offset: Optional[int] = 0
+
+    @validator("start_date", "end_date")
+    def validate_and_convert_to_utc(cls, value):
+        try:
+
+            if value is None:
+                return None
+            # Parse the datetime string
+            dt = datetime.fromisoformat('+'.join(value.split(' ')))
+
+            # Ensure the parsed datetime is in UTC
+            if dt.tzinfo is None or dt.tzinfo.utcoffset(dt) is None:
+                raise ValueError(
+                    "Datetime string should include timezone information")
+
+            # Convert the datetime to UTC using pytz
+            utc_timezone = pytz.UTC
+            dt_utc = dt.astimezone(utc_timezone)
+
+            return dt_utc
+        except ValueError:
+            raise ValueError("Invalid ISO datetime format")
 
 
 class SalesDataResponse(BaseModel):
-    date: datetime
-    sales_amount: float
-    product: str
-    category: str
+    id: int
+    user_id: int
+    transaction_timestamp: datetime
+    product_name: str
+    description: str
+    category_name: str
+    price: float
+    quantity: int
+
+    class Config:
+        from_attributes = True
 
 
 class InventoryStatusResponse(BaseModel):
